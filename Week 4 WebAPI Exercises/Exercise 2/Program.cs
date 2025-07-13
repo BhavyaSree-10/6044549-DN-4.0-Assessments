7e2eac525181a2559ad41b588e74c734c7151775
Bhavya <126454795+BhavyaSree-10@users.noreply.github.com>
2025-07-14 00:24:33 +0530

    Add files via upload

---
+++ Week 4 WebAPI Exercises/Exercise 2/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add Swagger generation service
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Swagger Demo",
+        Version = "v1",
+        Description = "TBD",
+        TermsOfService = new Uri("https://example.com"),
+        Contact = new OpenApiContact
+        {
+            Name = "John Doe",
+            Email = "john@xyzmail.com",
+            Url = new Uri("https://www.example.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License Terms",
+            Url = new Uri("https://www.example.com")
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint
+app.UseSwagger();
+
+// Enable middleware to serve Swagger UI
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo");
+});
+
+// Configure the HTTP request pipeline
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
